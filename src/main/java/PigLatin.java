a1e92c25f9ef703cf54a6f3f527d8deae5073464
Winstonshih <Winstonshih@github.com>
2026-09-01 23:25:38 +0000

    Test run: 3/3 passed, 0 failed, 0 skipped - 2026-09-01T23:25:38.811Z

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,7 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String pig=in.substring(1)+in.substring(0,1)+"ay";
+        return pig;
     }
 }
